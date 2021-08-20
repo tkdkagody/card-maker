da0109568410138d0370b193c338f18caa45539c
tkdkagody <tkdkagody@gmail.com>
2021-08-20 18:58:44 +0900

    mk repo

--- src/service/auth_service.js
+++ src/service/auth_service.js
@@ -3,7 +3,7 @@ import firebase from 'firebase' ;
 class AuthService {
     login(providerName){
         const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
-        return firebase.auth().signInWithPopup(authProvider)
+        return firebase.auth().signInWithPopup (authProvider)
     }
 }
 

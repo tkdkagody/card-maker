d29448b5241bb80dfa743ebc1cb1f3dde49ed291
tkdkagody <tkdkagody@gmail.com>
2021-08-21 13:42:20 +0900

    add card and editform compo

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import styles from './footer.module.css'
+import styles from './footer.module.css';
 
-const Footer = () => {
-    return(
-        <footer className={styles.footer}>
-            <p className={styles.title}>Code your dream</p>
-        </footer>
-    );
-}
+const Footer = () => (
+  <footer className={styles.footer}>
+    <p className={styles.title}>Code your dream</p>
+  </footer>
+);
 
 export default Footer;
